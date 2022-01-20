3c469433f472ecf44cb8613041076db595c8cfab
PHAGUN-JAIN <phagunjain@protonmail.com>
2022-01-20 17:07:15 +0530

    updated awesome route

--- src/routes/awesomeRoutes.js
+++ src/routes/awesomeRoutes.js
@@ -1,11 +1,8 @@
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.send("hey router");
+router.route("/").get((req, res) => {
+  // res.send("Hello World blah!!");
+  res.redirect("https://youtu.be/dQw4w9WgXcQ");
 });
-// router.route("/").get((req, res) => {
-//   res.send("Hello World blah!!");
-//   //   res.redirect("https://youtu.be/dQw4w9WgXcQ");
-// });
 
 module.exports = router;
